226ebfc5322837ab8578dca4dde9c47cbadb04cb
yonasghiwot <yonihabeshaw@gmail.com>
2024-03-02 00:43:14 +0300

    builtin.c

--- builtin.c
+++ builtin.c
@@ -22,53 +22,6 @@ void exit_shell(char **arv)
 	exit(0);
 }
 /**
- * _setenv - Initialize a new environment variable, or modify an existing one
- * @arv: array of entered words
- */
-void _setenv(char **arv)
-{
-	int i, j, k;
-
-	if (!arv[1] || !arv[2])
-	{
-		perror(_getenv("_"));
-		return;
-	}
-
-	for (i = 0; environ[i]; i++)
-	{
-		j = 0;
-		if (arv[1][j] == environ[i][j])
-		{
-			while (arv[1][j])
-			{
-				if (arv[1][j] != environ[i][j])
-					break;
-
-				j++;
-			}
-			if (arv[1][j] == '\0')
-			{
-				k = 0;
-				while (arv[2][k])
-				{
-					environ[i][j + 1 + k] = arv[2][k];
-					k++;
-				}
-				environ[i][j + 1 + k] = '\0';
-				return;
-			}
-		}
-	}
-	if (!environ[i])
-	{
-
-		environ[i] = concat_all(arv[1], "=", arv[2]);
-		environ[i + 1] = '\0';
-
-	}
-
-/**
  * _atoi - converts a string into an integer
  *@s: pointer to a string
  *Return: the integer
